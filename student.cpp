ea8246d1a7bfb9b20d4c186d82534b8c1b0891b4
Adam Pena <adam.bdl.pena@gmail.com>
2024-07-09 14:37:11 -0700

    Designate function definitions to student.h and implementations to student.cpp

--- student.cpp
+++ student.cpp
@@ -1,40 +1,7 @@
 #include <iostream>
-#include <string>
-#include "degree.h"  // Ensure this file is available and correctly defined
+#include "student.h"
 
-using namespace std;
-
-class Student {
-public:
-    Student(string studentID, string firstName, string lastName, string emailAddress, int age, int daysToComplete[3], DegreeProgram degreeProgram);
-    void setStudentID(string newID);
-    void setFirstName(string newFirstName);
-    void setLastName(string newLastName);
-    void setEmailAddress(string newEmail);
-    void setAge(int newAge);
-    void setDaysToComplete(int newArr[3]);
-    void setDegreeProgram(DegreeProgram newDegree);
-
-    string getStudentID() const;
-    string getFirstName() const;
-    string getLastName() const;
-    string getEmailAddress() const;
-    int getAge() const;
-    const int* getDaysToComplete() const;
-    DegreeProgram getDegreeProgram() const;
-    void print() const;
-
-private:
-    string studentID;
-    string firstName;
-    string lastName;
-    string emailAddress;
-    int age;
-    int daysToComplete[3];
-    DegreeProgram degreeProgram;
-};
-
-Student::Student(string studentID, string firstName, string lastName, string emailAddress, int age, int daysToComplete[3], DegreeProgram degreeProgram) {
+Student::Student(std::string studentID, std::string firstName, std::string lastName, std::string emailAddress, int age, int daysToComplete[3], DegreeProgram degreeProgram) {
     this->studentID = studentID;
     this->firstName = firstName;
     this->lastName = lastName;
@@ -46,19 +13,19 @@ Student::Student(string studentID, string firstName, string lastName, string ema
     this->degreeProgram = degreeProgram;
 }
 
-void Student::setStudentID(string newID) {
+void Student::setStudentID(std::string newID) {
     studentID = newID;
 }
 
-void Student::setFirstName(string newFirstName) {
+void Student::setFirstName(std::string newFirstName) {
     firstName = newFirstName;
 }
 
-void Student::setLastName(string newLastName) {
+void Student::setLastName(std::string newLastName) {
     lastName = newLastName;
 }
 
-void Student::setEmailAddress(string newEmail) {
+void Student::setEmailAddress(std::string newEmail) {
     emailAddress = newEmail;
 }
 
@@ -76,19 +43,19 @@ void Student::setDegreeProgram(DegreeProgram newDegree) {
     degreeProgram = newDegree;
 }
 
-string Student::getStudentID() const {
+std::string Student::getStudentID() const {
     return studentID;
 }
 
-string Student::getFirstName() const {
+std::string Student::getFirstName() const {
     return firstName;
 }
 
-string Student::getLastName() const {
+std::string Student::getLastName() const {
     return lastName;
 }
 
-string Student::getEmailAddress() const {
+std::string Student::getEmailAddress() const {
     return emailAddress;
 }
 
@@ -104,32 +71,18 @@ DegreeProgram Student::getDegreeProgram() const {
     return degreeProgram;
 }
 
-void Student::print() const{
-    std::cout << "Student ID: " << getStudentID()<< "\n"
-    << "First Name: " << getFirstName() << "\n"
-    << "Last Name: " << getLastName() << "\n"
-    << "Email Address: " << getEmailAddress() << "\n"
-    << "Age: " << getAge() << "\n"
-    << "Days to Complete Courses: {";
-    for(int i = 0; i < 3; i++){
-        std::cout << getDaysToComplete()[i];
-        if(i < 2){
+void Student::print() const {
+    std::cout << studentID << "\t"
+              << "First Name: " << firstName << "\t"
+              << "Last Name: " << lastName << "\t"
+              << "Age: " << age << "\t"
+              << "daysInCourse: {";
+    for (int i = 0; i < 3; i++) {
+        std::cout << daysToComplete[i];
+        if (i < 2) {
             std::cout << ", ";
         }
     }
-    std::cout << "}\n"
-    << "Degree Program: " <<  getDegreeProgram();
-}
-
-int main(int argc, char const *argv[])
-{
-    // Create an array of days to complete courses
-    int days[3] = {30, 45, 60};
-    
-    // Create a Student object
-    Student student("A123", "John", "Doe", "john.doe@example.com", 20, days, DegreeProgram::SOFTWARE);
-    
-    // Print the student's details
-    student.print();
-    return 0;
+    std::cout << "} "
+              << "Degree Program: " << degreeProgram << "\n";
 }
--- student.h
+++ student.h
@@ -1,88 +1,34 @@
-#include <stdio.h>
+#pragma once
+#include <string>
 #include "degree.h"
 
 class Student {
-    public:
-        void setStudentID(string newID);
-        void setFirstName(string newFirstName);
-        void setLastName(string newLastName);
-        void setEmailAddress(string newEmail);
-        void setAge(int newAge);
-        void setDaysToComplete(int newArr[3]);
-        void setDegreeProgram(DegreeProgram newDegree);
+public:
+    Student(std::string studentID, std::string firstName, std::string lastName, std::string emailAddress, int age, int daysToComplete[3], DegreeProgram degreeProgram);
 
-        string getStudentID();
-        string getFirstName();
-        string getLastName();
-        string getEmailAddress();
-        int getAge();
-        int getDaysToComplete();
-        DegreeProgram getDegreeProgram();
+    void setStudentID(std::string newID);
+    void setFirstName(std::string newFirstName);
+    void setLastName(std::string newLastName);
+    void setEmailAddress(std::string newEmail);
+    void setAge(int newAge);
+    void setDaysToComplete(int newArr[3]);
+    void setDegreeProgram(DegreeProgram newDegree);
 
-    private:
-        string studentID;
-        string firstName;
-        string lastName;
-        string emailAddress;
-        int age;
-        int[3] daysToComplete;
-        DegreeProgram degreeProgram;
-};
+    std::string getStudentID() const;
+    std::string getFirstName() const;
+    std::string getLastName() const;
+    std::string getEmailAddress() const;
+    int getAge() const;
+    const int* getDaysToComplete() const; // Return a const pointer to an array
+    DegreeProgram getDegreeProgram() const;
+    void print() const;
 
-void Student::setStudentID(string newID){
-    studentID = newID;
-};
-
-void Student::setFirstName(string newFirstName){
-    firstName = newFirstName;
-};
-
-void Student::setLastName(string newLastName){
-    lastName = newLastName;
-};
-
-void Student::setEmailAddress(string newEmail){
-    emailAddress = newEmail;
-};
-
-void Student::setAge(int newAge){
-    age = newAge;
-};
-
-void Student::setDaysToComplete(int newArr[3]){
-    for (int i=0; i<3; i++) {
-        daysToComplete[i] = newArr[i];
-    }
-};
-
-void Student::setDegreeProgram(DegreeProgram newDegree){
-    degreeProgram = newDegree;
-};
-
-string Student::getStudentID(){
-    return studentID;
-};
-
-string Student::getFirstName(){
-    return firstName;
-};
-
-string Student::getLastName(){
-    return lastName;
-};
-
-string Student::getEmailAddress(){
-    return emailAddress;
-};
-
-int Student::getAge(){
-    return age;
-};
-
-int[3] Student::getDaysToComplete(){
-    return daysToComplete;
-};
-
-DegreeProgram Student::getDegreeProgram(){
-    return degreeProgram;
+private:
+    std::string studentID;
+    std::string firstName;
+    std::string lastName;
+    std::string emailAddress;
+    int age;
+    int daysToComplete[3];
+    DegreeProgram degreeProgram;
 };
