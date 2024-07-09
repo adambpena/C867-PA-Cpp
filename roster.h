ee42cf8e04b566d07cf9244d50dcbd1959946b40
Adam Pena <adam.bdl.pena@gmail.com>
2024-07-09 14:37:45 -0700

    Add function declarations to roster.h

---
+++ roster.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include "student.h"
+
+class Roster
+{
+    public:
+        Student *classRosterArray[5];
+
+        void parse(std::string studentData); // Populates classRosterArray (calls Add to create Student objects and add them)
+        void add(std::string studentId, std::string firstName, std::string lastName, std::string emailAddress, int age, int daysInCourse1, int daysInCourse2, int daysInCourse3, DegreeProgram degreeProgram); // Called within parse
+        void remove(std::string studentId); // Destroy memory of student if found w/ "delete", if not report error "The student with the ID: {id}" was not found.
+        void printAll(); // Call each student's print message
+        void printAverageDaysInCourse(std::string studentId) // Find student w/ matching id and return averaged num of days in 3 courses
+        void printInvalidEmails() // Traverse through class roster and use Regex validation to validate. Return invalid emails
+        void printByDegreeProgram(DegreeProgram degreeProgram) // Call print method of students in roster w/ matching degree program
+}
