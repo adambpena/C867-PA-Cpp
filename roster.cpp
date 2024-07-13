999af26a03bbe2c6758988df6daa023b1bdd47a4
Adam Pena <adam.bdl.pena@gmail.com>
2024-07-13 10:58:44 -0700

    Add parse method (initial, untested)

--- roster.cpp
+++ roster.cpp
@@ -16,6 +16,59 @@ using namespace std;
 //         void printByDegreeProgram(DegreeProgram degreeProgram) // Call print method of students in roster w/ matching degree program
 // }
 
+std::string degreeProgramStrings = { "SECURITY", "NETWORK", "SOFTWARE" };
+
 void Roster::parse(std::string studentData){
+    size_t rhs = studentData.find(",");
+    std::string studentID = studentData.substr(0, rhs)
+
+    size_t lhs = rhs + 1
+    size_t rhs = studentData.find(",", lhs);
+    std::string firstName = studentData.substr(lhs, rhs-lhs);
+
+    size_t lhs = rhs + 1
+    size_t rhs = studentData.find(",", lhs);
+    std::string lastName = studentData.substr(lhs, rhs-lhs);
+
+    size_t lhs = rhs + 1
+    size_t rhs = studentData.find(",", lhs);
+    std::string emailAddress = studentData.substr(lhs, rhs-lhs);
+
+    size_t lhs = rhs + 1
+    size_t rhs = studentData.find(",", lhs);
+    int age = stoi(studentData.substr(lhs, rhs-lhs));
+
+    size_t lhs = rhs + 1
+    size_t rhs = studentData.find(",", lhs);
+    std::string daysInCourse1 = stoi(studentData.substr(lhs, rhs-lhs))
+
+    size_t lhs = rhs + 1
+    size_t rhs = studentData.find(",", lhs);
+    std::string daysInCourse2 = stoi(studentData.substr(lhs, rhs-lhs))
+
+    size_t lhs = rhs + 1
+    size_t rhs = studentData.find(",", lhs);
+    std::string daysInCourse3 = stoi(studentData.substr(lhs, rhs-lhs))
+
+    size_t lhs = rhs + 1
+    size_t rhs = studentData.find(",", lhs);
+    std::string degree = studentData.substr(lhs, rhs-lhs)
+    DegreeProgram degreeProgram; 
+
+    if (degree == "SECURITY") {
+        degreeProgram = DegreeProgram::SECURITY;
+    } else if (degree == "NETWORK") {
+        degreeProgram = DegreeProgram::NETWORK;
+    } else if (degree == "SOFTWARE") {
+        degreeProgram = DegreeProgram::SOFTWARE;
+    } else {
+        degreeProgram = DegreeProgram::UNKNOWN; // Handle invalid input
+    }
+
+    add(studentID, firstName, lastName, emailAddress, age, daysInCourse1, daysInCourse2, daysInCourse3, degreeProgram);
+
+};
+
+void Roster::add(std::string studentId, std::string firstName, std::string lastName, std::string emailAddress, int age, int daysInCourse1, int daysInCourse2, int daysInCourse3, DegreeProgram degreeProgram){
     
 };
